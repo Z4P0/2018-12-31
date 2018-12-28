23a8da06f829310bfbe74784c550c73883060d90
luis rosario <lrosario101@gmail.com>
2018-12-28 12:21:02 -0500

    canvas starter point

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-console.log('test');
+window.addEventListener('load', function(event) {
+    // find the canvas element
+    let canvasElement = document.querySelector('#canvas');
+
+    // have it fill it's parent container
+    let parentElement = canvasElement.parentNode;
+    canvasElement.setAttribute('width', parentElement.offsetWidth);
+    canvasElement.setAttribute('height', parentElement.offsetHeight);
+});
